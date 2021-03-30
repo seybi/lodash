f016d4d83e8ac2d22f50f4662b69841f8c67f0ed
Albert Sabitov <seybi@yandex.ru>
2021-03-30 11:37:48 +0300

    add chunk

---
+++ index.js
@@ -0,0 +1,32 @@
+const _ = require('lodash');
+
+let arr = ['a', 'b', 'c', 'd'];
+
+const chunk = function (array, size) {
+    let newArr = [];
+    let s = 1;
+    let arr = [];
+    if (size === undefined) {
+        size = 1;
+    } else if (size < 0) {
+        return newArr;
+    }
+
+    for (let i = 0; i < array.length; i++) {     
+        if (s <= size) {
+            arr.push(array[i]);
+        } else {
+            newArr.push(arr);
+            arr = [];
+            arr.push(array[i]);
+            s = 1;
+        }
+        s++;    
+    }
+    newArr.push(arr);
+
+    return newArr;
+}
+
+//console.log(_.chunk(['a', 'b', 'c', 'd', 'e'], 2));
+//console.log(chunk(['a', 'b', 'c', 'd', 'e'], 2));
